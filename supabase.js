4e82f92a90f8bf546d51dac5debeab5df36fc390
joshuaotieno4080-dev <joshuaotieno4080@gmail.com>
2026-02-27 13:43:57 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,27 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+// Paste your real values here:
+const supabaseUrl = "https:/sb_publishable__I-5Xru31gg0n-4MSbpOjA_yLmbrqwG";
+const supabaseKey = "sb_secret_cmlGfuvNgdAF2XZL6-UoPw_iSx4erWy";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Add product
+export async function addProduct(name, price, category, imageUrl) {
+    .from("products")
+    .insert([{ name, price, category, imageUrl }])
+
+// Delete product
+export async function deleteProduct(id) {
+    .from("products")
+    .delete()
+    .eq("id", id);
+}
+
+// Fetch products
+export async function getProducts(category) {
+  let query = supabase.from("products").select("*");
+  if (category) query = query.eq("category", category);
+  const { data, error } = await query;
+  return data;
+}
