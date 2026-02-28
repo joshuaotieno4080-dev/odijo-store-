a093bd65a92d22fc644ad65c2223a73847807720
joshuaotieno4080-dev <joshuaotieno4080@gmail.com>
2026-02-28 00:36:23 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,22 +1,20 @@
-// Initialize Supabase
 const { createClient } = supabase;
-const supabaseUrl = "https://sb_publishable__I-5Xru31gg0n-4MSbpOjA_yLmbrqwG;
-const supabaseKey = "sb_secret_cmlGfuvNgdAF2XZL6-UoPw_iSx4erWy";
+const supabaseUrl = "https://hncxmcpsiocoidljoayq.supabase.co";
+const supabaseKey = "sb_publishable__I-5Xru31gg0n-4MSbpOjA_yLmbrqwG";
 const db = createClient(supabaseUrl, supabaseKey);
 
-// Fetch products
-async function fetchProducts(category = null) {
-  let query = db.from("products").select("*");
-  if (category) query = query.eq("category", category);
-  return data;
+async function fetchProducts() {
+  const { data, error } = await db.from("products").select("*").order("id", { ascending: false });
+  if (error) console.error(error);
+  return data || [];
 }
 
-// Add product
 async function addProduct(product) {
-  const  = await db.from("products").insert([product]);
+  const { error } = await db.from("products").insert([product]);
+  if (error) console.error(error);
 }
 
-// Delete product
 async function deleteProduct(id) {
-  const  = await db.from("products").delete().eq("id", id
+  const { error } = await db.from("products").delete().eq("id", id);
+  if (error) console.error(error);
 }
