00074f75c7b4fb3d9e9e66813d2e3c049541f0bf
joshuaotieno4080-dev <joshuaotieno4080@gmail.com>
2026-02-27 23:34:15 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,27 +1,22 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-// Paste your real values here:
-const supabaseUrl = "https:/sb_publishable__I-5Xru31gg0n-4MSbpOjA_yLmbrqwG";
+// Initialize Supabase
+const { createClient } = supabase;
+const supabaseUrl = "https://sb_publishable__I-5Xru31gg0n-4MSbpOjA_yLmbrqwG;
 const supabaseKey = "sb_secret_cmlGfuvNgdAF2XZL6-UoPw_iSx4erWy";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
-// Add product
-export async function addProduct(name, price, category, imageUrl) {
-    .from("products")
-    .insert([{ name, price, category, imageUrl }])
-
-// Delete product
-export async function deleteProduct(id) {
-    .from("products")
-    .delete()
-    .eq("id", id);
-}
+const db = createClient(supabaseUrl, supabaseKey);
 
 // Fetch products
-export async function getProducts(category) {
-  let query = supabase.from("products").select("*");
+async function fetchProducts(category = null) {
+  let query = db.from("products").select("*");
   if (category) query = query.eq("category", category);
   return data;
 }
 
+// Add product
+async function addProduct(product) {
+  const  = await db.from("products").insert([product]);
+}
+
+// Delete product
+async function deleteProduct(id) {
+  const  = await db.from("products").delete().eq("id", id
+}
